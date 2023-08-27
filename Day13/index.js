1dbfa83f26edaec3b8fc63dc8e6c36acfcb315ed
Pavithraa AG <pavithraagopinath1706@gmail.com>
2023-08-27 09:08:57 +0530

    final commit

--- Day13/index.js
+++ Day13/index.js
@@ -1,71 +1,72 @@
-const express= require('express');
+const express = require('express');
 const path = require('path');
-const bodyParser=require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient
-const ObjectId=require("mongodb").ObjectId
-const client=new MongoClient("mongodb://0.0.0.0:27017/mydb");
+const ObjectId = require("mongodb").ObjectId
+const multer = require('multer')
+const client = new MongoClient("mongodb://0.0.0.0:27017/mydb");
 client.connect()
-.then(()=>{
-  console.log("connected");
-})
+  .then(() => {
+    console.log("connected");
+  })
 
 
 
 app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, '/src/public/','views'));
+app.set("views", path.join(__dirname, '/src/public/', 'views'));
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const db=client.db("mydb");
-var coll2=db.collection("user");
-var coll1=db.collection("blogs");
-app.get("/home", (req, res)=>{
-  
-      coll1.find({}).toArray()
-      .then((data)=>{
-        res.render("home",{data:data})
-      }).catch((err)=>{
-        console.log(err)
-      });
+const db = client.db("mydb");
+var coll2 = db.collection("user");
+var coll1 = db.collection("blogs");
+app.get("/home", (req, res) => {
+
+  coll1.find({}).toArray()
+    .then((data) => {
+      res.render("home", { data: data })
+    }).catch((err) => {
+      console.log(err)
+    });
 });
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.render("page")
 })
 
-app.get("/signin", (req, res)=>{
+app.get("/signin", (req, res) => {
   res.render("signin")
 })
-app.post('/signin_save', (req, res)=>{
-  const obj={name:req.body.username,password:req.body.password}
+app.post('/signin_save', (req, res) => {
+  const obj = { name: req.body.username, password: req.body.password }
   coll2.find(obj).toArray()
-      .then((data)=>{
-        res.redirect("/home")
-      }).catch((err)=>{
-        res.redirect("/signup")
-        console.log(err)
-      });
+    .then((data) => {
+      res.redirect("/home")
+    }).catch((err) => {
+      res.redirect("/signup")
+      console.log(err)
+    });
 });
-app.get("/signup", (req, res)=>{
+app.get("/signup", (req, res) => {
   res.render("signup")
 })
-app.post('/signup_save', (req, res)=>{
-  
-  const users={name:req.body.username,password:req.body.password}
+app.post('/signup_save', (req, res) => {
+
+  const users = { name: req.body.username, password: req.body.password }
   coll2.insertOne(users)
-  .then(()=>{
-    res.redirect("/")
-  }).catch((err)=>{
-    console.log(err)
-  })
+    .then(() => {
+      res.redirect("/")
+    }).catch((err) => {
+      console.log(err)
+    })
 });
 
-  
+
 
 // app.use(express.json());
 
 
-app.listen(8000,()=>{
+app.listen(8000, () => {
   console.log('listening on')
 })
--- Day14/index.js
+++ Day14/index.js
@@ -1,38 +1,65 @@
 // 1.create an api fetch a single document by id.
 // 2.upload file using postman. Validate it by checking the size of the file and the extension. size should not be more than 1mb and extension should be only pdf.
 
-var express = require('express');
-const app= express();
-const mongoose=require('mongoose');
-const bodyparser=require('body-parser');
-const routes=require('./src/routes/app');
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended:true}));
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const routes = require('./src/routes/app');
+const multer = require('multer');
+const path = require('path');
 
-mongoose.connect('mongodb://0.0.0.0:27017/mydb',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+const app = express();
 
-const db=mongoose.connection;
-db.once('open',function(){
-    console.log('mongoose connection')
-})
-app.use('/',routes)
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.listen(8000,()=>{
-    console.log("working")
-})
+mongoose.connect('mongodb://0.0.0.0:27017/mydb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+const DB = mongoose.connection;
+DB.on('open', () => {
+  console.log("Connected to the database");
+});
 
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    if (path.extname(file.originalname) === '.pdf') {
+      cb(null, 'data');
+    } else {
+      cb(new Error("This is not a PDF file"), false);
+    }
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  }
+});
 
+const upload = multer({
+  storage: multerStorage,
+  limits: {
+    fileSize: 1000000 
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') {
+      cb(null, true);
+    } else {
+      cb(new Error('Only PDF files are allowed'));
+    }
+  }
+});
 
+app.post("/upload", upload.single("myfile"), (req, res) => {
+  if (req.file) {
+    res.send('File uploaded successfully');
+  } else {
+    res.status(400).send('File upload failed');
+  }
+});
 
+app.use('/', routes);
 
-
-
-
-
-
-
-
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
